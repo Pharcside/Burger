d60d7f5dc99d71013d796e06dbdff37887001d03
Brian Corbett <briancorbett@brighthouse.com>
2019-07-09 16:32:54 -0400

    sql schema and seeds created. mysql burgers_db created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, devoured) VALUES ('Marilyn Manson', false);
+INSERT INTO burgers (name, devoured) VALUES ('Joe Biden', true);
